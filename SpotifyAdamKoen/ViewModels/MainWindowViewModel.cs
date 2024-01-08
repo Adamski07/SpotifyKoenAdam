2d038063fa0ab10ee6f73623c35f48aa48dd8dd2
Adamski07 <s1178952@student.windesheim.nl>
2024-01-08 17:40:09 +0100

    Add project files.

---
+++ SpotifyAdamKoen/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using SpotifyAK.Classes;
+using System.ComponentModel;
+
+namespace SpotifyAdamKoen.ViewModels
+{
+    public class MainWindowViewModel : NotifyPropertyChanged
+    {
+        private static MainWindowViewModel _instance;
+
+        public static MainWindowViewModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MainWindowViewModel();
+                }
+                return _instance;
+            }
+        }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            _currentView = new SongView();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SpotifyAdamKoen/ViewModels/NavBarViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+using CSharpLes42;
+using SpotifyAdamKoen;
+using SpotifyAK.Classes;
+
+namespace SpotifyAdamKoen.ViewModels
+{
+    public class NavBarViewModel : NotifyPropertyChanged
+    {
+        public ICommand NavigateToSongCommand { get; private set; }
+        public ICommand NavigateToAlbumCommand { get; private set; }
+
+        public NavBarViewModel()
+        {
+            NavigateToSongCommand = new RelayCommand(NavigateToSong);
+            NavigateToAlbumCommand = new RelayCommand(NavigateToAlbum);
+        }
+
+        private void NavigateToSong(object obj)
+        {
+            MainWindowViewModel.Instance.CurrentView = new SongView();
+        }
+
+        private void NavigateToAlbum(object obj)
+        {
+            MainWindowViewModel.Instance.CurrentView = new AlbumView();
+        }
+    }
+}
