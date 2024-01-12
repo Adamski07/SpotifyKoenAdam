5bc4157a206bcda554f7e016494ad1f279ff9fe7
Adamski07 <s1178952@student.windesheim.nl>
2024-01-12 17:27:11 +0100

    Model toegevoegd song

---
+++ SpotifyAdamKoen/Models/Song.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyAdamKoen.Classes;
+using System.Windows.Controls;
+
+namespace SpotifyAdamKoen.Models
+{
+    public class Song : NotifyPropertyChanged
+    {
+        private static int nextId = 1;
+
+        private int _id;
+        public int Id
+        {
+            get => this._id;
+            set
+            {
+                if (this._id != value)
+                {
+                    this._id = value;
+                    RaisePropertyChange(nameof(Id));
+                }
+            }
+        }
+
+        private string _artist;
+        public string Artist
+        {
+            get => this._artist;
+            set
+            {
+                this._artist = value;
+                RaisePropertyChange(nameof(Artist));
+            }
+        }
+
+        private string _genre;
+        public string Genre
+        {
+            get => this._genre;
+            set
+            {
+                this._genre = value;
+                RaisePropertyChange(nameof(Genre));
+            }
+        }
+
+        private DateTime _releaseDate;
+        public DateTime ReleaseDate
+        {
+            get => this._releaseDate;
+            set
+            {
+                this._releaseDate = value;
+                RaisePropertyChange(nameof(ReleaseDate));
+            }
+        }
+
+        private int _durationInSeconds;
+        public int DurationInSeconds
+        {
+            get => this._durationInSeconds;
+            set
+            {
+                this._durationInSeconds = value;
+                RaisePropertyChange(nameof(DurationInSeconds));
+            }
+        }
+
+    }
+}
